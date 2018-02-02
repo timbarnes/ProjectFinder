a60b51a47e4d6e9beb2d5e32115f14d8c7a960de
Tim Barnes <tw@timandmarina.com>
2018-02-01 21:07:44 -0800

    Destroy obsolete Buttons and bind <Enter> to GO.

--- ff.py
+++ ff.py
@@ -39,24 +39,26 @@ class Application(ttk.Frame):
         # Create the main frame
         ttk.Frame.__init__(self, master)
         # Create the communicating variables
-        self.search_string = tkinter.StringVar()
-        self.grid()
         self.createWidgets()
+        self.found_widgets = []
 
     def createWidgets(self):
         """
         Create the elements of the UI and connect to variables.
         """
+        self.grid()
+        self.search_string = tkinter.StringVar()
         # Frame with prompt, string, and go button
-        self.query_frame = ttk.Frame(self, borderwidth=2)
+        self.query_frame = ttk.Frame(self, borderwidth=5)
         self.query_frame.grid(row=0, column=0)
 
         self.prompt = ttk.Label(self.query_frame,
                                 text="Search for:", justify='right')
         self.prompt.grid(row=0, column=0)
 
-        self.search_string = ttk.Entry(self.query_frame,
+        self.search_string = ttk.Entry(self.query_frame, width=25,
                                        textvariable=self.search_string)
+        self.search_string.bind('<Return>', self.onEnter)
         self.search_string.grid(row=1, column=0)
 
         self.go = ttk.Button(self.query_frame, text='GO',
@@ -64,31 +66,33 @@ class Application(ttk.Frame):
         self.go.grid(row=2, column=0)
 
         self.photo_frame = ttk.LabelFrame(self, text='Photo Folders',
-                                          borderwidth=2, width=200)
+                                          borderwidth=5, width=200)
         self.photo_frame.grid(row=1, column=0)
 
         self.project_frame = ttk.LabelFrame(self, text='Project Folders',
-                                            borderwidth=2, width=200)
-        self.project_frame.grid(row=0, column=1)
+                                            borderwidth=5, width=200)
+        self.project_frame.grid(row=2, column=0)
+
+    def onEnter(self, event):
+        """
+        Hitting enter should be the same as clicking GO.
+        """
+        self.go_search()
 
     def launchWindow(self, match):
         """
         Create a window to view the folder matched.
         """
-        full_path = shlex.quote(os.path.join(PROJECT_BASE, match))
-        print(os.getcwd())
-        print("Launching folder <{}>".format(full_path))
-        subprocess.call('open {}'.format(full_path))
+        full_path = os.path.join(PROJECT_BASE, match)
+        subprocess.run(["open", full_path])
 
     @staticmethod
     def findMatches(f_list, search_str):
         """
         Return the list of folders that match the search_str.
         """
-        if len(search_str) > 1:
-            return [x for x in f_list if x.upper().count(search_str.upper()) > 0]
-        else:
-            error("Search needs at least two characters.")
+        return [x for x in f_list
+                if x.upper().count(search_str.upper()) > 0]
 
     def showResults(self, frame, matches):
         """
@@ -103,19 +107,32 @@ class Application(ttk.Frame):
                 command=lambda: self.launchWindow(match)))
             widget_list[-1].grid(row=i, column=0)
             i += 1
+        self.found_widgets += widget_list  # Save the created widgets
+
+    def destroyWidgets(self):
+        """
+        Delete any widgets in self.widget_list, because this is a new run.
+        """
+        for w in self.found_widgets:
+            w.destroy()
 
     def go_search(self):
         """
         Execute the search, and post results as widgets.
         """
-        pr_list = self.findMatches(PROJECT_LIST, self.search_string.get())
-        ph_list = self.findMatches(PHOTO_LIST, self.search_string.get())
-        if pr_list:
-            self.showResults(self.project_frame, pr_list)
-        if ph_list:
-            self.showResults(self.photo_frame, pr_list)
+        ss = self.search_string.get()
+        if len(ss) < 2:
+            error("Search string too short")
+        else:
+            pr_list = self.findMatches(PROJECT_LIST, ss)
+            ph_list = self.findMatches(PHOTO_LIST, ss)
+            self.destroyWidgets()  # Delete any previous results
+            if pr_list:
+                self.showResults(self.project_frame, pr_list)
+            if ph_list:
+                self.showResults(self.photo_frame, pr_list)
 
 
 app = Application()
-app.master.title('Project Create and Update')
+app.master.title('Project Finder')
 app.mainloop()
