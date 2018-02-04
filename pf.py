37dc8325d651c31d1d0f657806d8be4f9924bf83
Tim Barnes <tw@timandmarina.com>
2018-02-04 12:47:07 -0800

    Fixed launch to correct folder locations.

--- pf.py
+++ pf.py
@@ -75,11 +75,11 @@ class Application(ttk.Frame):
         """
         self.go_search()
 
-    def launchWindow(self, match):
+    def launchWindow(self, base, match):
         """
         Create a window to view the folder matched.
         """
-        full_path = os.path.join(PROJECT_BASE, match)
+        full_path = os.path.join(base, match)
         print(full_path)
         system = platform.system()
         if system == 'Windows':
@@ -97,7 +97,7 @@ class Application(ttk.Frame):
         return [x for x in f_list
                 if x.upper().count(search_str.upper()) > 0]
 
-    def showResults(self, frame, matches):
+    def showResults(self, frame, base, matches):
         """
         Generate buttons in _frame_ for _matches_.
         """
@@ -107,7 +107,7 @@ class Application(ttk.Frame):
         for match in matches:
             widget_list.append(ttk.Button(
                 frame, text=match, width=50,
-                command=functools.partial(self.launchWindow, match)))
+                command=functools.partial(self.launchWindow, base, match)))
             # command=lambda: self.launchWindow(match)))
             widget_list[-1].grid(row=i, column=0)
             i += 1
@@ -133,8 +133,8 @@ class Application(ttk.Frame):
             pr_list = self.findMatches(PROJECT_LIST, ss)
             ph_list = self.findMatches(PHOTO_LIST, ss)
             self.destroyWidgets()  # Delete any previous results
-            self.showResults(self.project_frame, pr_list)
-            self.showResults(self.photo_frame, ph_list)
+            self.showResults(self.project_frame, PROJECT_BASE, pr_list)
+            self.showResults(self.photo_frame, PHOTO_BASE, ph_list)
 
 
 def main():
